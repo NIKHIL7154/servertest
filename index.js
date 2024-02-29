a757133a3eac94f1b1d6b6cb5aa9f5c9f3fcb48e
NIKHIL <nikhilthakurq@gmail.com>
2024-02-29 22:32:57 +0530

    hello

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/good",(req,res)=>res.send("Everything is fair in love and war."))
 
 app.listen(5000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
